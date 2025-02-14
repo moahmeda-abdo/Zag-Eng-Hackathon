c73899d4f02915580904545c3dac77e1fc8bef0e
Mohamed Abdo <mohamedabdoelatar@gmail.com>
2025-02-14 14:52:08 +0200

    my orders

--- src/api/v1/routes/orders/index.ts
+++ src/api/v1/routes/orders/index.ts
@@ -4,8 +4,10 @@ import { DeleteOrderRoute } from './delete_order.route';
 import { GetOrdersRoute } from './get_orders.route';
 import { GetOrderDetailsRoute } from './get_order_details.route';
 import { UpdateOrderRoute } from './update_order.route';
+import { GetMyOrdersRoute } from './my_orders.route';
 const router = Router();
 
+router.use(GetMyOrdersRoute);
 router.use(CreateOrderRoute);
 router.use(UpdateOrderRoute);
 router.use(DeleteOrderRoute);
---
+++ src/api/v1/routes/orders/my_orders.route.ts
@@ -0,0 +1,25 @@
+
+import { Router } from 'express';
+import { Order } from '../../models/order/order.model';
+import { JOIValidateRequest } from "../../../../core/middleware/validation/joi-validate-request.middleware";
+import { ObjectIdRouteParamsValidationSchema } from "../../../common/validation/route_params.validation"
+import { NotFoundError } from '../../../../core/errors/not-found.error';
+import { Middleware } from "../../../common/types.common";
+
+
+const router = Router();
+
+
+const MyOrders: Middleware = async (req, res) => {
+    const userId = req.user._id;
+    const orders = await Order.find({ user: userId, is_deleted: false });
+    res.status(200).json({ status: 200, data: orders  , total: orders.length , message: "Orders Found Successfully", arMessage: "تم العثور على الطلبات بنجاح" })
+}
+
+router.get(
+    "/my-orders",
+    JOIValidateRequest(ObjectIdRouteParamsValidationSchema, "params"),
+    MyOrders,
+)
+
+export { router as GetMyOrdersRoute };
